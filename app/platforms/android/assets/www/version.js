14130b45c01a05c66e82a55a2bbc0887d4c16454
Andrés Vázquez <andres@data99.com.ar>
2017-03-19 18:49:15 -0300

    compilando

--- app/platforms/android/assets/www/version.js
+++ app/platforms/android/assets/www/version.js
@@ -1,2 +1,2 @@
 /*AUTOGENERADO POR EL COMPILADOR*/
-var cordova_app_version = "1.1.120";
+var cordova_app_version = "1.1.121";
--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,26 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
 
-    streamingSrc = "http://192.168.0.105:8000/radio.ogg", // "http://radio.data99.com.ar:8000/radio.ogg",
-    logoSrc = "http://mendiolaza.com.ar/wp-content/uploads/2017/03/logo-radio-mendiolaza.jpg",
+    streamingSrc: "http://192.168.0.105:8000/radio.ogg", // "http://radio.data99.com.ar:8000/radio.ogg",
+    logoSrc: "http://mendiolaza.com.ar/wp-content/uploads/2017/03/logo-radio-mendiolaza.jpg",
 
 
     initialize: function() {
@@ -34,13 +17,40 @@ var app = {
     onDeviceReady: function() {
         // listo el dispositivo
         console.log('Dispositivo listo: ');
-        // cargar la imagen y el audio
-        $("#streamaudio").attr("src", self.streamingSrc);
-        $("#logocentral").attr("src", self.logoSrc);
-        $("#appversion").html("Versión: " + cordova_app_version);
+        app.initapp();    
+    },
 
+    initapp: function(){
+        // cargar la imagen y el audio
+        this.tolog("Iniciando APP versión: " + cordova_app_version );
+        $("#logocentral").attr("src", this.logoSrc);
+        $("#appversion").html("Versión: " + cordova_app_version);
+        $("#streamaudio").attr("src", this.streamingSrc);
+        $("#audioctrl")[0].play();
+        netState = this.getNetworkState($("#audioctrl")[0].networkState);
+        this.tolog(netState);
+    },
+
+    getNetworkState: function(ns) {
+        if (ns == 0) return "0 = NETWORK_EMPTY - audio/video has not yet been initialized";
+        if (ns == 1) return "1 = NETWORK_IDLE - audio/video is active and has selected a resource, but is not using the network"
+        if (ns == 2) return "2 = NETWORK_LOADING - browser is downloading data"
+        if (ns == 3) return "3 = NETWORK_NO_SOURCE - no audio/video source found"
+    },
+    tolog: function(txt){
+        $("#mylogs").append("<p>" + txt + "</p>");
     },
 
 };
 
-app.initialize();
+$( document ).ready(function() {
+    var inCordova = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+    if (inCordova) {
+        app.initialize();    
+    }
+    else {
+        // empezar directo sin esperar el evento de Cordova
+        app.initapp();
+    }  
+});
+
